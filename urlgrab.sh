ad4cabb6c7c87697de21f82994eace10565bd779
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2022-11-05 16:55:28 -1000

    Add files via upload

--- urlgrab.sh
+++ urlgrab.sh
@@ -4,10 +4,13 @@
 # Usage example: ./script.sh 1 500
 
 JOBCOUNT=20 # Maximum concurrent requests/downloads
+ENDPOINT='http://localhost:5000/image/'
+START=$1
+END=$2
 
 run_curl() {
-	curl -f -C- -sSOJL http://localhost:5000/image/$1 &&
-	curl -s -X DELETE http://localhost:5000/image/$1 -o /dev/null
+	curl -f -C- -sSOJL $ENDPOINT$1 &&
+	curl -sX DELETE $ENDPOINT$1 -o /dev/null
 	if [ $? -eq 0 ]
 	then
 		RESULT="." # Success
@@ -17,25 +20,21 @@ run_curl() {
 	echo -n $RESULT
 }
 
-START=$1
-END=$2
-
 cd "$(dirname $0)" # TODO: Check for mounted drive, cd to mounted directory
 echo "Downloading to $(pwd)"
 
 while [ $START -lt $(($END+1)) ]
 do
 	# Run function while process count is below threshold
-	if [ $(pgrep curl | wc -l) -lt $JOBCOUNT ]
+	if [ $(/usr/bin/pgrep curl 2>/dev/null | wc -l) -lt $JOBCOUNT ]
 	then
 		run_curl $START &
 		START=$(($START+1))
 	else
 		sleep 0.1
 	fi
-
 done
 
 wait
-echo -e "\nScript finished."
+echo -e "\nDone!"
 
