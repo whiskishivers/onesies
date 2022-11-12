ee481dc34accac911b91275eea659a75dc6a700d
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2022-11-11 23:40:47 -1000

    Add files via upload

--- urlgrab.sh
+++ urlgrab.sh
@@ -3,37 +3,28 @@
 # Download files and then delete them using the web API.
 # Usage example: ./script.sh 1 500
 
-JOB_LIMIT=30 # Maximum concurrent requests/downloads
-BASE_URL='http://localhost:5000/record'
-STARTING_DIR=$(pwd)
-START=$1
-END=$2
+#TODO: Check for mounted drive, exit if not found
+
+BASE_URL="http://localhost:5000/record"
+DOWNLOAD_DIR=$(pwd)
+JOB_LIMIT=30
 
 run_curl() {
-	# Download data, then send delete request.
 	DATA_URL="$BASE_URL/$1/data"
 	DELETE_URL="$BASE_URL/$1"
-
-	(curl -f -C- -sSOJL $DATA_URL &&
-	 curl -sX DELETE $DELETE_URL -o /dev/null &&
-	 echo -n ".")
+	cd $DOWNLOAD_DIR
+	curl -f -sSOJL $DATA_URL &&
+	curl -sX DELETE $DELETE_URL -o /dev/null &&
+	echo -n "."
 }
 
-# TODO: Need correct path for mounted drive before this works...
-#if [ -n "$(mount | grep " on /media/$USER/label")" ]
-#then
-#	cd /media/$USER/label
-#else
-#	echo "Hard drive is not mounted, or mounted to the wrong dir."
-#	exit 1
-#fi
-
-cd $(dirname $0)
-echo "Starting downloads to $(pwd)..."
-
+# Run a job for each number in the range.
+# Wait if the job count meets the limit.
+START=$1
+END=$2
+echo "Downloading to $DOWNLOAD_DIR..."
 while [ $START -lt $(($END+1)) ]
 do
-	# Run function while process count is below threshold
 	JOB_COUNT=$(jobs -r | wc -l)
 	if [ $JOB_COUNT -lt $JOB_LIMIT ]
 	then
@@ -46,6 +37,5 @@ do
 done
 
 wait
-cd $STARTING_DIR
-echo -e "\nDone!"
+echo "Done!"
 
