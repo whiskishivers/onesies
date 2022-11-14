bbb9ec23240238ea40b73c49467a873049297acb
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2022-11-14 10:01:10 -1000

    Add files via upload

--- urlgrab.sh
+++ urlgrab.sh
@@ -4,41 +4,45 @@
 # Usage example: ./script.sh 1 500
 
 BASE_URL="http://localhost:5000/record"
-DOWNLOAD_DIR="/media/mint/elements" # Full path to mounted hard drive
-JOB_LIMIT=50
+MOUNT_DIR="/media/$USER/elements" # Full path to mounted hard drive
+DOWNLOAD_DIR="$(hostname)_$(date +%Y%m%d_%H%M)"
+JOB_LIMIT=25
 
 run_curl() {
 	DATA_URL="$BASE_URL/$1/data"
 	DELETE_URL="$BASE_URL/$1"
-	cd $DOWNLOAD_DIR
 	(curl -f -sSOJL $DATA_URL &&
-	curl -sX DELETE $DELETE_URL -o /dev/null &&
-	echo -n ".") ||
+	curl -sX DELETE $DELETE_URL -o /dev/null) ||
 	sleep 1
 }
 
-# Run only if the dir has a mounted device
-if [ -z "$(mount | grep "on $DOWNLOAD_DIR type")" ]
-then
-	echo "No mounted device found for $DOWNLOAD_DIR!"
+# Check for mounted device.
+[ -z "$(mount | grep "on $MOUNT_DIR type")" ] &&
+	echo "No device mounted to $MOUNT_DIR" &&
 	exit 1
-fi
 
 START=$1
 END=$2
 JOBCOUNT_PEAK=0
-echo "Downloading to $DOWNLOAD_DIR..."
+
+# Create download directory on device
+cd $MOUNT_DIR
+mkdir -p $DOWNLOAD_DIR
+cd $DOWNLOAD_DIR
+
+echo "Downloading to $(pwd)"
 while [ $START -lt $(($END+1)) ]
 do
 	# Run curl if the job limit isn't reached
 	JOB_COUNT=$(jobs -r | wc -l)
 	if [ $JOB_COUNT -lt $JOB_LIMIT ]
 	then
+		echo -n "."
 		run_curl $START &
 		START=$(($START+1))
 	else
-		#wait $!
-		sleep 0.3
+		#wait
+		sleep 0.1
 	fi
 	
 	# Performance testing stuff
@@ -49,5 +53,6 @@ do
 done
 
 wait
+ls -lh
 echo "Done!"
-# echo $JOBCOUNT_PEAK
+#echo "Job peak: $JOBCOUNT_PEAK"
