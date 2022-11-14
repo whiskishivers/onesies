80308275d3e4fe74e0d760f1b22018b0365a3a2c
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2022-11-13 20:15:43 -1000

    Add files via upload

--- urlgrab.sh
+++ urlgrab.sh
@@ -1,41 +1,53 @@
 #!/bin/bash
 
-# Download files and then delete them using the web API.
+# Download files to an attached hard drive, then delete off disk using the web API.
 # Usage example: ./script.sh 1 500
 
-#TODO: Check for mounted drive, exit if not found
-
 BASE_URL="http://localhost:5000/record"
-DOWNLOAD_DIR=$(pwd)
-JOB_LIMIT=30
+DOWNLOAD_DIR="/media/mint/elements" # Full path to mounted hard drive
+JOB_LIMIT=50
 
 run_curl() {
 	DATA_URL="$BASE_URL/$1/data"
 	DELETE_URL="$BASE_URL/$1"
 	cd $DOWNLOAD_DIR
-	curl -f -sSOJL $DATA_URL &&
+	(curl -f -sSOJL $DATA_URL &&
 	curl -sX DELETE $DELETE_URL -o /dev/null &&
-	echo -n "."
+	echo -n ".") ||
+	sleep 1
 }
 
-# Run a job for each number in the range.
-# Wait if the job count meets the limit.
+# Run only if the dir has a mounted device
+if [ -z "$(mount | grep "on $DOWNLOAD_DIR type")" ]
+then
+	echo "No mounted device found for $DOWNLOAD_DIR!"
+	exit 1
+fi
+
 START=$1
 END=$2
+JOBCOUNT_PEAK=0
 echo "Downloading to $DOWNLOAD_DIR..."
 while [ $START -lt $(($END+1)) ]
 do
+	# Run curl if the job limit isn't reached
 	JOB_COUNT=$(jobs -r | wc -l)
 	if [ $JOB_COUNT -lt $JOB_LIMIT ]
 	then
 		run_curl $START &
 		START=$(($START+1))
 	else
-		# wait $!
-		sleep 0.1
+		#wait $!
+		sleep 0.3
+	fi
+	
+	# Performance testing stuff
+	if [ $JOB_COUNT -gt $JOBCOUNT_PEAK ]
+	then
+		JOBCOUNT_PEAK=$JOB_COUNT
 	fi
 done
 
 wait
 echo "Done!"
-
+# echo $JOBCOUNT_PEAK
