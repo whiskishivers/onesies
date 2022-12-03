37358924b7408d28aac98cf3ea59ead83768d21d
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2022-12-03 09:15:57 -1000

    Add files via upload

--- storage-check.sh
+++ storage-check.sh
@@ -3,22 +3,23 @@
 # If a filesystem is nearing a storage limit percentage, do things
 
 MOUNTPOINT=/data
-STORAGE_LIMIT=90
+STORAGE_LIMIT=99
 
+pgrep storage-check | grep -v $$ && exit
+
+# Check for mounted filesystem
+findmnt $MOUNTPOINT > /dev/null || exit 1
 
 # Report usage, and exit if not over the limit
-
-USED=$(df $MOUNTPOINT --output=pcent | tail -n 1 | awk '{ print $1 }' | sed 's/%//' || exit 1)
-
+USED=$(findmnt $MOUNTPOINT -f -n -o USE% | awk '{ print $1 }' | sed 's/%//' || exit 1)
 if [ $USED -lt $STORAGE_LIMIT ]
 then
-	echo "$(date +%D\ %r) - $MOUNTPOINT usage: $USED%. Exiting."
+	echo "$(date +%D\ %r) - $MOUNTPOINT is at $USED%. Exiting."
 	exit 0
 else
-	echo "$(date +%D\ %r) - $MOUNTPOINT usage: $USED%. Storage limit ($STORAGE_LIMIT%) exceeded."
+	echo "$(date +%D\ %r) - $MOUNTPOINT is at $USED%. Storage limit ($STORAGE_LIMIT%) exceeded!"
 fi
 
 
 ### Storage is over the limit, code to do things goes here ###
-
-df -h $MOUNTPOINT
+echo "oh lawd its over the limit"
