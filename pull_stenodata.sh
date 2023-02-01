898381dfbf34a8fd0549d1d81386188c4dea9460
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2023-01-31 18:17:53 -1000

    Add files via upload

--- pull_stenodata.sh
+++ pull_stenodata.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-# Connect to remote servers, copy stenographer data and index files to local directories.
+# Pull stenographer data from each server to local directories.
 
-SERVERS=( "10.1.3.102,steno2" "10.1.3.103,steno3" "10.1.3.104,steno4" "10.1.3.105,steno5" "10.1.3.106,steno6" )
+SERVERS=(
+	"10.1.1.12,steno2"
+	"10.1.1.13,steno3"
+	"10.1.1.14,steno4"
+	"10.1.1.15,steno5"
+	"10.1.1.16,steno6"
+)
 
+run_rsync() {
+	# Args: $1: IP, $2: Local dir
+	rsync -r --stats --exclude=".*" --chmod=F644 ops@$1:/data/stenographer/ $2 &&
+	echo "[*] Successful $1 pull."
+}
+
+# Start rsync job for each server.
 for i in ${SERVERS[@]}
 do
 	IFS=',' read -r -a array <<< "$i"
 	IP="${array[0]}"
 	HOSTNAME="${array[1]}"
-	echo "Started pull from $IP."
-	rsync -r --exclude=".*" --chmod=F644 ops@$IP:/data/stenographer/packets/ /data/$HOSTNAME/steno/packets &
-	rsync -r --exclude=".*" --chmod=F644 ops@$IP:/data/stenographer/index/ /data/$HOSTNAME/steno/index &
 
-	# Limit to four concurrent jobs
+	echo "[ ] Starting $IP pull."
+	run_rsync $IP /data/$HOSTNAME/steno &
+
+	# Limit concurrent jobs
 	while [ $(jobs -r | wc -l) -ge 4 ]
 	do
-		sleep 0.1
+		sleep 1
 	done
+
 done
 
 wait
