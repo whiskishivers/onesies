7ea4411f6e6c47a85572b7ca0e0e9f3c1f709749
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2023-05-26 17:37:31 -1000

    Add files via upload

--- dl-dependencies.sh
+++ dl-dependencies.sh
@@ -1,13 +1,19 @@
 #bin/bash
 
-# Download a deb package with required dependencies to disk. Not recursive!
+# Download a package from the repo, including dependencies
 
 if [ -z $1 ]; then 
 	echo "Usage: ./dl-dependencies.sh <pkg>"
 	exit 1;
 fi
 
-apt-get download $1 && \
+
+TOPDIR=$(realpath $(dirname $0))
+
+mkdir -p $TOPDIR/$1/dependencies &&
+cd $TOPDIR/$1 &&
+apt-get download $1 &&
+cd dependencies
 apt-get download $(apt-cache depends $1 | grep "Depends:" | awk '{print $2}')
 
 # Install downloaded packages
