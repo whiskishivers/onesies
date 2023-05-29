7ac7c2873709857719c7bf66c34cebf539ea0760
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2023-05-28 15:07:46 -1000

    Update dl-dependencies.sh

--- dl-dependencies.sh
+++ dl-dependencies.sh
@@ -1,26 +1,28 @@
-#bin/bash
+#!/bin/bash
 
 # Download packages found in pkgs.txt and all dependencies (recursive).
 
 get_deb() {
-	echo " * Downloading $1..."
-	apt-get download -qq $1 2>/dev/null&&
-	for i in $(apt-rdepends -s DEPENDS $1 | grep "^\w" | sort -u);
-	do
-		apt-get download -qq $i 2>/dev/null
-	done
+        echo " * Downloading $1..."
+        apt-get download -qq $1 2>/dev/null &&
+        cd dependencies &&
+        for i in $(apt-rdepends -s DEPENDS $1 | grep "^\w" | sort -u);
+        do
+                apt-get download -qq $i 2>/dev/null
+        done
 }
-
 
 apt-get update || exit 1
 
 echo "Downloading packages found in pkgs.txt..."
 
+mkdir -p dependencies
+
 for i in $(cat pkgs.txt); do
-	get_deb $i &
-	while [ $(jobs -r | wc -l) -gt 9 ]; do
-		sleep 0.1
-	done
+        get_deb $i &
+        while [ $(jobs -r | wc -l) -gt 9 ]; do
+                sleep 0.1
+        done
 done
 wait
 echo "Script finished."
