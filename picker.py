c23756dcae6c9c8ec4bf4a64c1fa1e93d28faca4
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2023-10-24 20:36:31 -1000

    Update picker.py

--- picker.py
+++ picker.py
@@ -7,29 +7,34 @@ import random
 # Lower points are more likely to be selected.
 # Output is in selection order.
 
+class Watchstander:
+	def __init__(self, name, points):
+		self.name = name
+		self.points = points
+	
+	def __str__(self):
+		return f"{self.name.ljust(15)} {self.points}"
 
-def get_score(n, highest):
-	# Subtract points from the highest in the pool. Adds one to prevent scores of zero.
-	return highest - n + 1
+def weigh_in(n, highscore):
+	return highscore - n + 1
 
-# Read file, put names and points into seperate lists
+# Read file, make list
 watchstanders = []
-points = []
 with open('book1.csv','r') as f:
+	f.readline() # skip header row
 	reader = csv.reader(f.readlines())
 	for row in reader:
-		watchstanders.append(f"{row[1].ljust(4)}{row[0]}")
-		points.append(int(row[1]))
+		watchstanders.append(Watchstander(row[0],int(row[1])))
 
-# Calculate scores and make selections
-highest = max(points)
-scores = [get_score(i, highest) for i in points]
-ws_count = len(watchstanders)
 selected = []
-while len(selected) < ws_count:
-	pick = random.sample(watchstanders, k=1, counts=scores)[0]
+while len(watchstanders) > 0:
+	# Calculate relative weights, make selection
+	highscore = max([w.points for w in watchstanders])
+	weights = [weigh_in(w.points, highscore) for w in watchstanders]
+	pick = random.choices(watchstanders, weights=weights)[0]
 	idx = watchstanders.index(pick)
-	for i in watchstanders, points, scores:
-		del i[idx]
 	selected.append(pick)
+	
 	print(pick)
+	del watchstanders[idx]
+	del weights[idx]
