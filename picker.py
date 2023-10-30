fef8afd4666adcd6b35161afe6d3b9e232def591
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2023-10-29 15:35:11 -1000

    Update picker.py

--- picker.py
+++ picker.py
@@ -4,7 +4,7 @@ import csv
 import random
 
 """
-Randomly picks watchstanders from a csv file where column 0 is name, column 1 is points.
+Randomly picks watchstanders from a csv file. Column 0 is name, column 1 is points.
 Watchstanders with lower points are more likely to be picked. Output is in selection order.
 """
 
@@ -13,12 +13,18 @@ def weigh_in(n, highscore):
 
 # Read file, make list
 watchstanders = []
-with open('book1.csv','r') as f:
-	f.readline() # skip header row
+with open('book1.csv', 'r') as f:
 	reader = csv.reader(f.readlines())
 	for row in reader:
 		watchstanders.append(row)
 
+# Skip row header
+row_header = None
+if "name" in watchstanders[0][0].lower():
+	row_header = watchstanders[0]
+	del watchstanders[0]
+
+# Make selections
 points = [int(w[1]) for w in watchstanders]
 highscore = max(points)
 weights = [weigh_in(i, highscore) for i in points]
@@ -26,8 +32,16 @@ selected = []
 while len(watchstanders) > 0:
 	pick = random.choices(watchstanders, weights=weights)[0]
 	idx = watchstanders.index(pick)
+	selected.append(pick)
+	
+	print(f"{pick[0]}")
 	del watchstanders[idx]
 	del weights[idx]
 	
-	selected.append(pick)
-	print(f"{pick[0]}")
+# Save results
+with open('selected.csv', 'w') as f:
+	writer = csv.writer(f)
+	if row_header is not None:
+		writer.writerow(row_header)
+	writer.writerows(selected)
+print("Selection saved to selected.csv")
