8d90736d8fbf9c3487d2dfb594b2b1b49830ad2b
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2023-11-23 21:53:55 -1000

    Update picker-buckets.py

--- picker-buckets.py
+++ picker-buckets.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 import csv
-from itertools import zip_longest
 from random import choices
 from statistics import median
+from itertools import zip_longest
 
 """
 Made to assist in watchbill creation. Reads a csv file that includes a
@@ -15,9 +15,9 @@ likely to be selected before higher scores.
 Creates a new csv file with the rows in order by selection.
 """
 
-# Pick ratio: x,y. Pick x low group members before y high group members.
+# Low:high pick ratio: x,y
+# Pick x low group members before y high group members.
 pick_ratio = 5,1
-
 
 class Watchstander:
 	def __init__(self, csvrow: list):	
@@ -34,53 +34,36 @@ class Watchstander:
 	def weight(self):
 		return max_score - self.points + 1
 
-
 # Make list
 rows = []
 with open('book1.csv', 'r') as f:
 	reader = csv.reader(f.readlines())
-	rows = [i for i in reader]
+	rows = list(reader)
 header_row = rows[0]
 watchstanders = [Watchstander(i) for i in rows[1:]]
 
-# Make groups, stats
+# Make groups
 points = [i.points for i in watchstanders]
 max_score, median_score = max(points), median(points)
 low_bucket = [i for i in watchstanders if i.points < median_score]
 high_bucket = [i for i in watchstanders if i.points >= median_score]
-print(f"Median score: {str(median_score).rjust(4)}")
-print("Groups:")
-for a,b in zip_longest(low_bucket, high_bucket):
-	print(f"{a} | {b}")
-print("")
 
-# Make selection
+# Make selections
 print("---Selection Order---")
 selected = []
 while len(low_bucket) + len(high_bucket) > 0:
-	# Low
-	try:
-		for i in range(pick_ratio[0]):
-			low_pick = choices(low_bucket, weights=[w.weight for w in low_bucket])[0]
-			del low_bucket[low_bucket.index(low_pick)]
-			selected.append(low_pick)
-			print(f"{low_pick} ")
-	except IndexError:
-		pass
-	# High
-	try:
-		for i in range(pick_ratio[1]):
-			high_pick = choices(high_bucket, weights=[w.weight for w in high_bucket])[0]
-			del high_bucket[high_bucket.index(high_pick)]
-			selected.append(high_pick)
-			print(f"{high_pick} " + ("." * int((high_pick.points / median_score))))
-	except IndexError:
-		pass
-
+	for bucket, ratio in ((low_bucket, pick_ratio[0]), (high_bucket, pick_ratio[1])):
+		try:
+			for _ in range(ratio):
+				pick = choices(bucket, weights=[w.weight for w in bucket])[0]
+				del bucket[bucket.index(pick)]
+				selected.append(pick)
+				print(pick)
+		except IndexError:
+			continue
 # Save selection order
 with open('selected.csv', 'w') as f:
 	writer = csv.writer(f)
 	writer.writerow(header_row)
 	writer.writerows([i.row for i in selected])
-	print("---")
-	print("Output saved to selected.csv")
+print("Output saved to selected.csv")
