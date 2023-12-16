02560da173df850ce7796df1a6a92cfb7d9b6367
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2023-12-15 15:48:03 -1000

    Update picker-buckets.py

--- picker-buckets.py
+++ picker-buckets.py
@@ -6,31 +6,37 @@ from statistics import median
 
 """
 Made to assist in watchbill creation. Reads a csv file that includes a
-header row. Column 0 must be Name and Column 1 must be points. Splits
-watchstanders by median points, then randomly picks based on the defined
-ratio and points. Lower scores are more likely to be selected before
-higher scores.
+header row with at least "Name" and "Points" columns. Splits watchstanders
+by median points, then randomly picks based on the defined ratio and points.
+Lower scores are more likely to be selected before higher scores.
 Creates a new csv file with the rows in order of selection.
 """
 
-# Selection ratio low to high
-PICK_RATIO = (5, 1)
+# Select x from the lower score group before selecting y from the higher group.
+x: int = 5
+y: int = 1
+
+PICK_RATIO = (x, y)
 
 
 class Watchstander:
-    def __init__(self, csvrow: list):
+    def __init__(self, csvrow):
         self.row = csvrow
 
     def __str__(self):
-        return f"{self.name.ljust(15)} {str(self.points).rjust(3)}"
+        return f"{self.name.ljust(15)} {str(self.points).rjust(3)}  {self.notes}"
 
     @property
     def name(self):
-        return self.row[0]
+        return self.row["Name"]
+
+    @property
+    def notes(self):
+        return self.row["Notes"]
 
     @property
     def points(self):
-        return int(self.row[1])
+        return int(self.row["Points"])
 
     @property
     def weight(self):
@@ -39,10 +45,9 @@ class Watchstander:
 
 def read_csv(file_path: str):
     with open(file_path, 'r') as f:
-        reader = csv.reader(f)
-        header_row = next(reader)
+        reader = csv.DictReader(f)
         watchstanders = [Watchstander(i) for i in reader]
-    return header_row, watchstanders
+    return watchstanders
 
 
 def make_selection(watchstanders, pick_ratio):
@@ -71,8 +76,8 @@ def make_selection(watchstanders, pick_ratio):
 
 def save_csv(file_path: str, header_row: list, selected: list) -> None:
     with open(file_path, 'w', newline='') as f:
-        writer = csv.writer(f)
-        writer.writerow(header_row)
+        writer = csv.DictWriter(f)
+        writer.writeheader()
         writer.writerows([i.row for i in selected])
 
 
@@ -80,7 +85,7 @@ def main():
     input_file = 'book1.csv'
     output_file = 'selected.csv'
 
-    header_row, watchstanders = read_csv(input_file)
+    watchstanders = read_csv(input_file)
     selected, med_score = make_selection(watchstanders, PICK_RATIO)
 
     print("---Selection Order---")
@@ -88,7 +93,7 @@ def main():
     print(f"Median: {med_score}")
     print(f"Count: {len(watchstanders)}")
 
-    save_csv(output_file, header_row, selected)
+    save_csv(output_file, selected)
     print("Output saved to", output_file)
 
 
