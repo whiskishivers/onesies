9b43800a73c11d42c38daf8b07cb73d65445a521
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2024-01-27 14:17:16 -1000

    Update kibana-filter.py

--- kibana-filter.py
+++ kibana-filter.py
@@ -4,32 +4,31 @@ import argparse
 import json
 
 """
-Creates a Kibana filter for matching one field to
-many values from one or more text files.
-Originally for zeek logs, but the field name can be modified.
+Creates a Kibana filter that matches one field to many values in text files.
 """
 
 parser = argparse.ArgumentParser(
-    description="Create a Kibana filter for matching a field to values in lines from text files."
+    description="Creates a Kibana filter that matches one field to many values in text files.",
+    epilog="Text files must have one field value per line. Duplicate lines are ignored."
 )
-parser.add_argument("--field", default="related.ip", help="The field to match (default: related.ip)")
-parser.add_argument("filepath", nargs="+", help="Path to the text file")
+parser.add_argument("--field", default="related.ip", help="field to match (default: related.ip)")
+parser.add_argument("--pretty", action="store_true", help="format output for readability")
+parser.add_argument("filepath", nargs="+", help="text file to read values from")
 args = parser.parse_args()
 
 all_lines = set()
 phrases = []
-for path in args.filepath:
-    with open(path, 'r') as f:
+
+for p in args.filepath:
+    with open(p, 'r') as f:
         all_lines.update(i.strip() for i in f.readlines())
 
+query = {"query": {"bool": {"should": [], "minimum_should_match": 1}}}
+
 for line in all_lines:
-    phrases.append({"match_phrase": {args.field: line}})
+    query["query"]["bool"]["should"].append({"match_phrase": {args.field: line}})
 
-qry = {"query": {
-    "bool": {
-        "should": phrases,
-        "minimum_should_match": 1  # is this default?
-    }
-}}
-
-print(json.dumps(qry))
+indent = None
+if args.pretty:
+    indent = 1
+print(json.dumps(query, indent=indent))
