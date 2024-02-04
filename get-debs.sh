bc39e5a8a5f21e5233ae06274bd46711f1c0e1e4
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2024-02-03 19:33:23 -1000

    Update get-debs.sh

--- get-debs.sh
+++ get-debs.sh
@@ -5,16 +5,9 @@
 touch uris.txt
 touch installed.txt
 mkdir -p debs
-
-# Make a list of repo uris to download
-apt list --installed | cut -d"/" -f1 | tail -n +2 > installed.txt
-xargs apt-get install --reinstall --print-uris < installed.txt | grep ^\'http | cut -d"'" -f2 > uris.txt
-
-# Download packages and make required repo files
 cd debs
-echo "Downloading new files..."
-xargs -a ../uris.txt wget -N -nv
-cd ..
+dpkg -l | grep "^ii" | awk '{print $2}' | xargs -n 20 apt-get download
+
 echo "Creating repository index..."
 apt-ftparchive packages . > Packages
 apt-ftparchive release . > Release
